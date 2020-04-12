4762dd5bc4c3b559d4dd132172c3b8cc8f98bcd0
Lefa G <saad.guessous@insa-lyon.fr>
2020-04-12 19:00:55 +0200

    searchBarDemo

--- src/control/auth/authRouter.ts
+++ src/control/auth/authRouter.ts
@@ -2,6 +2,7 @@ import * as winston from "winston";
 import { Router } from "express";
 import { LoginController } from "./LoginController";
 import { LogoutController } from "./LogoutController";
+import { AdvancedSearchController } from "./advancedSearchController";
 
 /**
  * Router for Authentication needs
@@ -11,11 +12,14 @@ export function authRouter(): Router {
   winston.debug("Mapping Authentication routes");
   const loginController: LoginController = new LoginController();
   const logoutController: LogoutController = new LogoutController();
+  const advancedSearchController: AdvancedSearchController = new  AdvancedSearchController();
   const router: Router = Router();
 
   router.get("/login", loginController.viewLoginForm);
   router.post("/login", loginController.login);
   router.get("/logout", logoutController.logout);
+  router.get("/advanced-search", advancedSearchController.advancedSearch);
+
 
   return router;
 }
